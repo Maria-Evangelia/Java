d241d96a7d0db8339712c7ba311db4efa41de032
Maria-Evangelia <134435816+Maria-Evangelia@users.noreply.github.com>
2023-06-09 21:33:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let myAge = 25
+if (myAge >= 20) {
+    console.log("my age is bigger or equal than 20");
+}  else if (myAge < 15)
+    console.log ("my age is less than 20");
+else if (myAfe > 20)
+console.log ("my age ist less than 25");
